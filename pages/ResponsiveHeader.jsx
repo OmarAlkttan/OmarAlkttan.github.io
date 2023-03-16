44a7f891b51c32309a3fe20b2ddde1e7b6777b2d
Omar Alktan <oalktan@gmail.com>
2023-03-16 20:29:06 +0200

    add header

---
+++ pages/ResponsiveHeader.jsx
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+function ResponsiveHeader(){
+
+  const [name, setName] = useState("");
+  const orgName = "I'm Omar Alktan."
+
+  useEffect(()=>{
+    if(name === orgName){
+      return ;
+    }
+    const id = setInterval(()=>{
+      setName(name => name.concat(orgName.at(name.length)))
+    }, 150)
+    
+    return ()=> clearInterval(id);
+  }, [name])
+
+  return (
+    <h1 className="text-white text-9xl font-bold text-center text-shadow-black text-shadow-lg">
+      <span>{name}</span>
+    </h1>
+  )
+}
+
+export default ResponsiveHeader;
